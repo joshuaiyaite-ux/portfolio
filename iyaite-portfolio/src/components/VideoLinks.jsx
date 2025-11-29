7029329380f9b76b3ff9c0eeff1b6336cb2b2ba2
Joshua Iyaite <joshuaiyaite@gmail.com>
2025-11-29 21:01:04 +0000

    Fix missing component and CSS files

---
+++ iyaite-portfolio/src/components/VideoLinks.jsx
@@ -0,0 +1,7 @@
+export default function VideoLinks() {
+  return (
+    <section>
+      {/* Video Links content */}
+    </section>
+  );
+}
---
+++ iyaite-portfolio/src/components/WorkPreview.jsx
@@ -0,0 +1,7 @@
+export default function WorkPreview() {
+  return (
+    <section>
+      {/* Work Preview content */}
+    </section>
+  );
+}
